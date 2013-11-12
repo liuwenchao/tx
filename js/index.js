4ee2b87aef2dbb4251584e8ce4be5ebde983156c
Louis Liu <louis.wenchao.liu@gmail.com>
2013-11-12 14:09:18 +0800

    fix js error.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 (function($){
-    var TX = {
+    TX = {
         bars: null,
         set_bars: function(bars) {
             this.bars = [];
             for (i=0; i<bars; i++) {
-                bars.push({
+                this.bars.push({
                     height: (Math.random() * 30) + 10
                 });
             }
         },
         build_bars: function(container, bars) {
-            var container = $(container),
+            var container = $(container), that = this;
             this.set_bars(bars);
             
             $.each(this.bars, function() {
-                container.append(this.build_html(this));
+                container.append(that.build_html(this));
             });
         },
         build_html: function(bar) {
